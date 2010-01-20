0b73f1504f49ac52904d0bb7a4d2da185f1bd586
virtualdc@mail.ru <virtualdc@mail.ru@5a6d72ec-0354-11df-adb7-1be7f7e3f636>
2010-01-20 23:53:46 +0000

    rearrange platform specific code
    git-svn-id: https://instead-launcher.googlecode.com/svn/trunk@110 5a6d72ec-0354-11df-adb7-1be7f7e3f636

--- platform.cpp
+++ platform.cpp
@@ -11,41 +11,36 @@ bool checkOrCreateGameDir( QString gameDir ) {
     return dir.mkpath( gameDir );
 }
 
-QString getGameDirPath()
-{
 #ifdef Q_OS_UNIX
+
+QString getGameDirPath() {
     return QDir::home().absolutePath() + "/.instead/games/";
-
-#elif defined(Q_OS_WIN)
-    return QDir::toNativeSeparators(QDir::home().absolutePath()) + "\\Local Settings\\Application Data\\instead\\games\\";
-
-#else
-#error "Unsupported OS"
-#endif
 }
 
-QString getConfigPath()
-{
-#ifdef Q_OS_UNIX
+QString getConfigPath() {
     return QDir::home().absolutePath() + "/.instead/launcher.ini";
-
-#elif defined(Q_OS_WIN)
-    return QDir::toNativeSeparators(QDir::home().absolutePath()) + "\\Local Settings\\Application Data\\instead\\launcher.ini";
-
-#else
-#error "Unsupported OS"
-#endif
 }
 
-QString getDefaultInterpreterPath()
-{
-#ifdef Q_OS_UNIX
+QString getDefaultInterpreterPath() {
     return "/usr/local/bin/sdl-instead";
+}
 
 #elif defined(Q_OS_WIN)
+
+QString getGameDirPath() {
+    return QDir::toNativeSeparators(QDir::home().absolutePath()) + "\\Local Settings\\Application Data\\instead\\games\\";
+}
+
+QString getConfigPath() {
+    return QDir::toNativeSeparators(QDir::home().absolutePath()) + "\\Local Settings\\Application Data\\instead\\launcher.ini";
+}
+
+QString getDefaultInterpreterPath() {
     return "c:\\Program Files\\Pinebrush games\\INSTEAD\\sdl-instead.exe";
+}
 
 #else
+
 #error "Unsupported OS"
+
 #endif
-}
