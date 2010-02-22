f8db4d04fdbc14184867646c97c85fc15a5b9da5
p.kosyh <p.kosyh@5a6d72ec-0354-11df-adb7-1be7f7e3f636>
2010-02-22 14:56:18 +0000

    tarball.sh fix
    git-svn-id: https://instead-launcher.googlecode.com/svn/trunk@188 5a6d72ec-0354-11df-adb7-1be7f7e3f636

--- tarball.sh
+++ tarball.sh
@@ -2,6 +2,8 @@
 VERSION="0.1"
 VERTITLE="instead-launcher-$VERSION"
 ARCHIVE="instead-launcher_$VERSION.tar.gz"
+qmake
+make clean
 svn st | grep "^?" | awk '{ print $$2 }' | while read l; do rm -rf $l; done
 ln -sf ./ "$VERTITLE"
 tar -cz --exclude "$VERTITLE/$VERTITLE" --exclude .svn --exclude tarball.sh --exclude "$ARCHIVE" -f "$ARCHIVE" "$VERTITLE"/*
