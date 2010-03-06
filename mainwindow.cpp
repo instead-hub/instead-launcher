5df718234af675f37d39f5b1e4eb6324a2909fc3
Ilja.Ryndin <Ilja.Ryndin@5a6d72ec-0354-11df-adb7-1be7f7e3f636>
2010-03-06 22:44:27 +0000

    cosmetic fixes
    git-svn-id: https://instead-launcher.googlecode.com/svn/trunk@213 5a6d72ec-0354-11df-adb7-1be7f7e3f636

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -104,7 +104,7 @@ MainWindow::MainWindow(QWidget *parent)
     m_ui->listGames->setAlternatingRowColors( true );
     m_ui->listNewGames->setAlternatingRowColors( true );
 
-    setWindowTitle( "INSTEAD LAUNCHER - "LAUNCHER_VERSION );
+    setWindowTitle( "INSTEAD launcher - "LAUNCHER_VERSION );
     setWindowIcon( QIcon( ":/resources/icon.png" ) );
 
     resetConfig();
