bd8b607c22e3a7dc72e7bba83c6f5256cd111041
Ilja.Ryndin <Ilja.Ryndin@5a6d72ec-0354-11df-adb7-1be7f7e3f636>
2010-03-09 08:39:59 +0000

    fixed about
    git-svn-id: https://instead-launcher.googlecode.com/svn/trunk@221 5a6d72ec-0354-11df-adb7-1be7f7e3f636

--- aboutwidget.cpp
+++ aboutwidget.cpp
@@ -8,12 +8,14 @@ AboutWidget::AboutWidget( QWidget *parent )
     QHBoxLayout *hlayout = new QHBoxLayout();
 
     QWidget *gwidget = new QWidget( this );
+    gwidget->setMinimumWidth( 400 );
     QPalette pal = gwidget->palette();
     pal.setBrush( gwidget->backgroundRole(), QBrush( QColor( 255, 255, 255 ) ) );
     gwidget->setPalette( pal );
     gwidget->setAutoFillBackground( true );
 
     QVBoxLayout *glayout = new QVBoxLayout( gwidget );
+    glayout->setSizeConstraint( QLayout::SetFixedSize );
 
     QWidget *lwidget = new QWidget( this );
     pal = lwidget->palette();
@@ -41,25 +43,32 @@ AboutWidget::AboutWidget( QWidget *parent )
     frame->setFrameShape( QFrame::HLine );
     ghvlayout->addWidget( frame );
     QLabel *title = new QLabel( "<b><h3>" + QString( "%1 %2" ).arg( tr( "INSTEAD launcher" ) ).arg( LAUNCHER_VERSION ) + "</h3></b>", this );
+    title->setScaledContents( false );
     ghvlayout->addWidget( title );
     frame = new QFrame( gwidget );
     frame->setFrameShape( QFrame::HLine );
     ghvlayout->addWidget( frame );
     QLabel *desc = new QLabel( tr( "The program for loading and installing games from the official INSTEAD games repository. Also launcher allows you to be in course of all related software updates." ), this );
     desc->setWordWrap( true );
+    desc->setScaledContents( false );
     ghvlayout->addSpacing( 10 );
     ghvlayout->addWidget( desc );
     QCommandLinkButton *developersForumButton = new QCommandLinkButton( tr( "Developer's forum" ), tr( "Join us! Let's discuss, make suggestions and enjoy!" ), lwidget );
+//    developersForumButton->setSizePolicy( QSizePolicy( QSizePolicy::MinimumExpanding, QSizePolicy::MinimumExpanding ) );
+//    developersForumButton->setMinimumHeight( 150 );
     connect( developersForumButton, SIGNAL( clicked() ), this, SLOT( goToDevelopersForum() ) );
     ghvlayout->addWidget( developersForumButton );
     QCommandLinkButton *launcherProjectButton = new QCommandLinkButton( tr( "INSTEAD launcher project page" ), tr( "If you want to report about bug, you can do it here." ), lwidget );
+//    launcherProjectButton->setSizePolicy( QSizePolicy( QSizePolicy::MinimumExpanding, QSizePolicy::Fixed ) );
     connect( launcherProjectButton, SIGNAL( clicked() ), this, SLOT( goToINSTEADLauncherPage() ) );
     ghvlayout->addWidget( launcherProjectButton );
     QCommandLinkButton *insteadProjectButton = new QCommandLinkButton( tr( "INSTEAD project page" ), tr( "Welcome to the INSTEAD project page!" ), lwidget );
+//    insteadProjectButton->setSizePolicy( QSizePolicy( QSizePolicy::MinimumExpanding, QSizePolicy::Fixed ) );
     connect( insteadProjectButton, SIGNAL( clicked() ), this, SLOT( goToINSTEADPage() ) );
     ghvlayout->addWidget( insteadProjectButton );
     QLabel *devs = new QLabel( "<center><b>" + tr( "Developers team" ) + ":</b><br>morkow, rec, <a href=\"mailto:ilja.ryndin@gmail.com\">" + tr( "Ilja Ryndin" ) +"</a></center>", this );
     devs->setWordWrap( true );
+    devs->setScaledContents( false );
     ghvlayout->addSpacing( 10 );
 //    ghlvlayout->addWidget( devs );
     ghvlayout->addWidget( devs );
@@ -84,6 +93,8 @@ AboutWidget::AboutWidget( QWidget *parent )
     ghlvlayout->addStretch();
 
     connect( checkUpdatesButton, SIGNAL( clicked() ), this, SIGNAL( checkUpdatesRequest() ) );
+
+
 }
 
 AboutWidget::~AboutWidget()
