897430581361d948595fadb90035135c3a4f015e
p.kosyh <p.kosyh@5a6d72ec-0354-11df-adb7-1be7f7e3f636>
2010-03-12 07:55:47 +0000

    ui fixes
    git-svn-id: https://instead-launcher.googlecode.com/svn/trunk@230 5a6d72ec-0354-11df-adb7-1be7f7e3f636

--- instead-launcher_ru.ts
+++ instead-launcher_ru.ts
@@ -128,10 +128,10 @@
         <translation>Убедитесь, что интепретатор был установлен</translation>
     </message>
     <message>
+        <location filename="mainwindow.ui" line="124"/>
         <location filename="mainwindow.cpp" line="425"/>
         <location filename="mainwindow.cpp" line="658"/>
         <location filename="mainwindow.cpp" line="704"/>
-        <location filename="mainwindow.ui" line="121"/>
         <source>all</source>
         <translation>все</translation>
     </message>
@@ -205,174 +205,176 @@
         <translation>Установленные игры</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="47"/>
-        <location filename="mainwindow.ui" line="193"/>
+        <location filename="mainwindow.ui" line="50"/>
+        <location filename="mainwindow.ui" line="205"/>
         <source>Game</source>
         <translation>Игра</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="52"/>
-        <location filename="mainwindow.ui" line="198"/>
+        <location filename="mainwindow.ui" line="55"/>
+        <location filename="mainwindow.ui" line="210"/>
         <source>Version</source>
         <translation>Версия</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="62"/>
+        <location filename="mainwindow.ui" line="65"/>
         <source>Remove</source>
         <translation>Удалить</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="82"/>
+        <location filename="mainwindow.ui" line="85"/>
         <source>Play !</source>
         <translation>Играть !</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="92"/>
+        <location filename="mainwindow.ui" line="95"/>
         <source>New games</source>
         <translation>Новые игры</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="113"/>
+        <location filename="mainwindow.ui" line="116"/>
         <source>Language</source>
         <translation>Язык</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="126"/>
+        <location filename="mainwindow.ui" line="129"/>
         <source>ru</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="131"/>
+        <location filename="mainwindow.ui" line="134"/>
         <source>en</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="136"/>
+        <location filename="mainwindow.ui" line="139"/>
         <source>es</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="243"/>
+        <location filename="mainwindow.ui" line="255"/>
         <source>Games path:</source>
         <translation>Путь к играм:</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="264"/>
+        <location filename="mainwindow.ui" line="276"/>
         <source>Additional INSTEAD parameters:</source>
         <translation>Дополнительные параметры INSTEAD:</translation>
     </message>
     <message>
-        <source>Check game updates on start</source>
-        <translation type="obsolete">Проверять обновления игр при запуске</translation>
-    </message>
-    <message>
-        <source>Check program updates on start</source>
-        <translation type="obsolete">Проверять обновления программ при запуске</translation>
-    </message>
-    <message>
-        <location filename="mainwindow.ui" line="148"/>
+        <location filename="mainwindow.ui" line="151"/>
         <source>Refresh list</source>
         <oldsource>Refresh</oldsource>
         <translation>Обновить список</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="168"/>
+        <location filename="mainwindow.ui" line="171"/>
         <source>Install</source>
         <translation>Установить</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="203"/>
+        <location filename="mainwindow.ui" line="215"/>
         <source>Project website</source>
         <translation>Страница проекта</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="212"/>
+        <location filename="mainwindow.ui" line="224"/>
         <source>Preferences</source>
         <translation>Настройки</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="220"/>
+        <location filename="mainwindow.ui" line="232"/>
         <source>Interpreter path:</source>
         <translation>Путь к интерпретатору:</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="236"/>
-        <location filename="mainwindow.ui" line="253"/>
+        <location filename="mainwindow.ui" line="248"/>
+        <location filename="mainwindow.ui" line="265"/>
         <source>Browse</source>
         <translation>Обзор</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="280"/>
+        <location filename="mainwindow.ui" line="320"/>
+        <source>Update</source>
+        <translation>Обновления</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="326"/>
         <source>Autostart actions</source>
         <translation>Действия при запуске</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="288"/>
+        <location filename="mainwindow.ui" line="334"/>
         <source>Check game updates</source>
         <translation>Проверка обновлений игр</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="295"/>
+        <location filename="mainwindow.ui" line="341"/>
         <source>Check program updates</source>
         <translation>Проверка обновлений программ</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="307"/>
+        <location filename="mainwindow.ui" line="353"/>
         <source>Update sources</source>
-        <translation>Источники обновления</translation>
+        <translation>Источники обновлений</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="331"/>
+        <location filename="mainwindow.ui" line="391"/>
         <source>Add</source>
         <translation>Добавить</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="338"/>
+        <location filename="mainwindow.ui" line="398"/>
         <source>Delete</source>
         <translation>Удалить</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="350"/>
+        <location filename="mainwindow.ui" line="416"/>
+        <source>Network</source>
+        <translation>Сеть</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="424"/>
         <source>Proxy</source>
         <translation>Прокси</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="364"/>
+        <location filename="mainwindow.ui" line="438"/>
         <source>Server:</source>
         <translation>Сервер:</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="377"/>
+        <location filename="mainwindow.ui" line="451"/>
         <source>127.0.0.1</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="400"/>
+        <location filename="mainwindow.ui" line="458"/>
         <source>Port:</source>
         <translation>Порт:</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="413"/>
+        <location filename="mainwindow.ui" line="471"/>
         <source>3128</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="422"/>
+        <location filename="mainwindow.ui" line="480"/>
         <source>Authentication</source>
         <translation>Аутентификация</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="433"/>
+        <location filename="mainwindow.ui" line="491"/>
         <source>User:</source>
         <translation>Пользователь:</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="463"/>
+        <location filename="mainwindow.ui" line="505"/>
         <source>Password:</source>
         <translation>Пароль:</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="500"/>
+        <location filename="mainwindow.ui" line="307"/>
         <source>Reset</source>
         <translation>Сбросить</translation>
     </message>
@@ -390,43 +392,43 @@
         <translation>Загрузка списка обновлений</translation>
     </message>
     <message>
-        <location filename="updatewidget.cpp" line="77"/>
+        <location filename="updatewidget.cpp" line="78"/>
         <source>Loading updates ... please wait</source>
         <translation>Поиск обновлений ... пожалуйста подождите</translation>
     </message>
     <message>
-        <location filename="updatewidget.cpp" line="112"/>
+        <location filename="updatewidget.cpp" line="113"/>
         <source>Error</source>
         <translation>Ошибка</translation>
     </message>
     <message>
-        <location filename="updatewidget.cpp" line="112"/>
+        <location filename="updatewidget.cpp" line="113"/>
         <source>Can&apos;t download update list. If you use proxy, check the proxy settings.</source>
         <translation>Невозможно получить список обновлений. Если вы используете соединение через прокси, проверьте настройки.</translation>
     </message>
     <message>
-        <location filename="updatewidget.cpp" line="163"/>
+        <location filename="updatewidget.cpp" line="164"/>
         <source>Instead update to </source>
         <translation>Обновление instead до </translation>
     </message>
     <message>
-        <location filename="updatewidget.cpp" line="165"/>
+        <location filename="updatewidget.cpp" line="166"/>
         <source>Instead update from </source>
         <translation>Обновление instead с </translation>
     </message>
     <message>
-        <location filename="updatewidget.cpp" line="165"/>
-        <location filename="updatewidget.cpp" line="171"/>
+        <location filename="updatewidget.cpp" line="166"/>
+        <location filename="updatewidget.cpp" line="172"/>
         <source> to </source>
         <translation> до </translation>
     </message>
     <message>
-        <location filename="updatewidget.cpp" line="171"/>
+        <location filename="updatewidget.cpp" line="172"/>
         <source>Launcher update from </source>
         <translation>Обновление launcher с</translation>
     </message>
     <message>
-        <location filename="updatewidget.cpp" line="182"/>
+        <location filename="updatewidget.cpp" line="183"/>
         <source>No updates available</source>
         <translation>Нет доступных обновлений</translation>
     </message>
